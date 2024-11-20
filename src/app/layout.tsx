3426312b0e7946adbae278337f1ef4257fae44fa
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-11-20 16:33:12 +0700

    edit metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "AI Startup Landing Page",
-  description: "A landing page for an AI startup created with Frontend Tribe",
+  title: "AI Startup Landing Page by lilbee",
+  description: "A landing page for an AI startup=",
 };
 
 export default function RootLayout({
